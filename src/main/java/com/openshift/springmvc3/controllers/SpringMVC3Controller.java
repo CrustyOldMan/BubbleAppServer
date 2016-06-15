055ba38498e8332e8b98997ea32c9eeb782043de
Chris Barrett <chris@chris-barrett.ca>
2016-06-14 23:23:53 -0700

    Fixed controller packaging

--- src/main/java/com/openshift/springmvc3/controllers/SpringMVC3Controller.java
+++ src/main/java/com/openshift/springmvc3/controllers/SpringMVC3Controller.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package server;
+package com.openshift.springmvc3.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
